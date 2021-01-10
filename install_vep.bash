e0e188d0659165b5603a61bca98c89c752805972
Lucio Montero <lucioric2000@hotmail.com>
2021-01-10 07:23:11 +0100

    Added Bash installer

---
+++ install_vep.bash
@@ -0,0 +1,5 @@
+sudo apt install gcc g++ git make
+conda install -y -c bioconda perl-dbi perl-dbd-mysql perl-archive-zip perl-json perl-set-intervaltree perl-bio-bigfile
+conda install -y -c bioconda curl htslib perl-bio-db-hts libcurl libssh2 perl-base perl-cgi perl-compress-raw-bzip2
+conda install -y -c bioconda perl-encode perl-html-parser perl-html-tagset perl-io-compress perl-scalar-list-utils
+conda install -y -c bioconda perl-sereal perl-sereal-decoder perl-sereal-encoder perl-xsloader unzip
